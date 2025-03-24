fc44b0585353243b529ab48dbb634445805d6aa1
Ermek <2304.01013@manas.edu.kg>
2025-03-24 23:20:00 +0600

    строки с 6 - 18 добавлены возможности для фронтенда. а ещё исправление get запросов

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from .routes import clients, payments, auth
-
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="Газ Контроль API")
-
+@app.get("/")
+def read_root():
+    return {"message": "Сервер работает!"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешить все домены (["http://localhost:3000"] для фронта)
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы (GET, POST, PUT, DELETE и т.д.)
+    allow_headers=["*"],  # Разрешить все заголовки
+)
+@app.get("/favicon.ico")
+async def favicon():
+    return {"message": "No favicon"}
 app.include_router(clients.router)
 app.include_router(payments.router)
 app.include_router(auth.router)
